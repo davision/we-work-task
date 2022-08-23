26c493630297855cd086a737d57bd404a4f3eb97
davision <david@artnetik.si>
2022-08-23 20:01:33 +0200

    Update UsernameField unit test

--- src/components/__tests__/UsernameField.spec.js
+++ src/components/__tests__/UsernameField.spec.js
@@ -5,6 +5,10 @@ import UsernameField from "../UsernameField.vue";
 
 describe("UsernameField", () => {
   it("renders properly", () => {
+    mount(UsernameField, {});
+  });
+
+  it("shows form label", () => {
     const wrapper = mount(UsernameField);
     expect(wrapper.text()).toContain("Username");
   });
